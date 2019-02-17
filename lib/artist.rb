4621fc05e5c4e6e3d5f4d12334550c9baba092a3
Yonnick Brown <25hour24@gmail.com>
2019-02-17 19:28:09 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,6 +1,10 @@
 class Artist
   attr_accessor :name, :song, :artist
 
+  def initialize(name)
+    @name = name
+  end
+  
   def find_or_create_by_name
     if (self.artist.nil?)
       self.artist = Artist.new(name)
